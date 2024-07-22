6b3c9279924fdba5b41aea46c407399583998337
saurabhsp <smpakhale36@gmail.com>
2024-07-22 21:02:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const IPL = ["CSK", "MI", "RCB", "SRH", "DC"];
+  const result = IPL.map((team) => <h1>{team}</h1>);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">{result}</div>
+    </>
   );
 }
 
